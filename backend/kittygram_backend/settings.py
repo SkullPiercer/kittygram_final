28fe981ab8e6b3e7c126c7aac714d58621afee25
Alexander <akatsuki_sanya@vk.com>
2024-07-23 23:51:43 +0500

    Migrations now working

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -58,7 +58,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django-user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
